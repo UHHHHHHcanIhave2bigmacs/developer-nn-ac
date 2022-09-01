e507742abf8b804f5d485882e458d17739238a3e
UHHHHHHcanIhave2bigmacs <108996145+UHHHHHHcanIhave2bigmacs@users.noreply.github.com>
2022-08-31 23:57:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-import { world } from "mojang-minecraft";
- 
-world.events.tick.subscribe(() => {
-  for (let player of world.getPlayers()) {
-	console.warn('Welcome to Neon Anticheat, we are currently testing out the auto-updating system, just sit and wait!')
-  };
-});
+console.warn('Welcome to Neon Anticheat, we are currently testing out the auto-updating system, just sit and wait!')
